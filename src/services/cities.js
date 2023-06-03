69cc71ba8d93037200327fbc166753dc4cbc1cc2
mateopautassoPC <mateopautasso.mg@gmail.com>
2023-06-03 14:18:21 -0300

    Intento de solución en url de api geonames

--- src/services/cities.js
+++ src/services/cities.js
@@ -3,7 +3,7 @@ import { request } from "../tools/request";
 export const getCities = async (countryCode) => {
     const optionsRequest = {
         method: 'GET',
-        url: 'http://api.geonames.org/searchJSON',
+        url: 'https://api.geonames.org/searchJSON',
         params: {
             country: countryCode ?? 'AR',
             maxRows: 1000,
