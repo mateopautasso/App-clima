f606788aa0d48f8ae6a71280330c50009c6ff934
mateopautassoPC <mateopautasso.mg@gmail.com>
2023-06-06 19:28:58 -0300

    Correción detalles

--- src/services/weather.js
+++ src/services/weather.js
@@ -3,7 +3,7 @@ import { request } from "../tools/request";
 export const getWeather = async (lat, lng) => {
     const optionsRequest = {
         method: 'GET',
-        url: 'http://api.weatherapi.com/v1/forecast.json?key=219bd80bc25a4d1f958180547230306&days=5&aqi=no&alerts=no',
+        url: 'https://api.weatherapi.com/v1/forecast.json?key=219bd80bc25a4d1f958180547230306&days=5&aqi=no&alerts=no',
         params: {
             lang: 'es',
             q: `${lat},${lng}`
